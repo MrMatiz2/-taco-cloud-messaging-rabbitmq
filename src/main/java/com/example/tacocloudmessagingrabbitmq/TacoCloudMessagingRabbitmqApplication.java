0672912b970254633062d9c2ff31d0bf62191766
MrMatiz2 <alejandromatiz87@gmail.com>
2024-11-30 21:58:22 -0500

    Initial commit

---
+++ src/main/java/com/example/tacocloudmessagingrabbitmq/TacoCloudMessagingRabbitmqApplication.java
@@ -0,0 +1,13 @@
+package com.example.tacocloudmessagingrabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloudMessagingRabbitmqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TacoCloudMessagingRabbitmqApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tacocloudmessagingrabbitmq/TacoCloudMessagingRabbitmqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tacocloudmessagingrabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudMessagingRabbitmqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
