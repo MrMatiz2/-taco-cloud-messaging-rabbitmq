3248d5606224c4f471a1a501db171327385b4c93
MrMatiz2 <alejandromatiz87@gmail.com>
2025-01-10 21:36:13 -0500

    Deploying and building Spring refactor

--- src/main/java/com/example/tacocloudmessagingrabbitmq/services/listeners/OrderListenerRabbit.java
+++ src/main/java/com/example/tacocloudmessagingrabbitmq/services/listeners/OrderListenerRabbit.java
@@ -1,6 +1,7 @@
 package com.example.tacocloudmessagingrabbitmq.services.listeners;
 
 import com.example.tacoclouddomain.entities.TacoOrder;
+import com.example.tacoclouddomain.kitchen.KitchenOrder;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
@@ -8,15 +9,15 @@ import org.springframework.stereotype.Component;
 @Component
 public class OrderListenerRabbit {
 
-    private final KitchenOrderRabbit kitchenOrderRabbit;
+    private final KitchenOrder kitchenOrder;
 
-    public OrderListenerRabbit(KitchenOrderRabbit kitchenOrderRabbit) {
-        this.kitchenOrderRabbit = kitchenOrderRabbit;
+    public OrderListenerRabbit(KitchenOrder kitchenOrder) {
+        this.kitchenOrder = kitchenOrder;
     }
 
     @RabbitListener(queues = "tacocloud.queue")
     public void receiveOrder(TacoOrder order) {
-        kitchenOrderRabbit.displayOrder(order);
+        kitchenOrder.displayOrder(order);
     }
 
 }
